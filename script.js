faddb31e20b492190ad5fd9e7ddba9494a5d5935
anderdev-GitHub <andersondiasleite@icloud.com>
2023-10-11 21:44:48 -0300

    melhorias na estilização e arualização de script

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-function geradorNumero() {
+const customAlert = document.getElementById('custom-alert');
+const alertMessage = document.getElementById('alert-message');
+const closeAlert = document.getElementById('close-alert');
 
-    const input1 = Math.ceil(document.querySelector('.input1').value)
-    const input2 = Math.floor(document.querySelector('.input2').value)
+const showCustomAlert = (message) => {
+    alertMessage.innerText = message;
+    customAlert.style.display = 'block';
+};
+
+closeAlert.addEventListener('click', () => {
+    customAlert.style.display = 'none';
+});
+
+const geradorNumero = () => {
+    const input1 = Math.ceil(document.querySelector('.input1').value);
+    const input2 = Math.floor(document.querySelector('.input2').value);
 
     const result = Math.floor(Math.random() * (input1 - input2 + 1)) + input2;
 
-    alert(result)
+    showCustomAlert(result);
+
+    if (result === 0) {
+        showCustomAlert("Escolha os números");
+        customAlert.style.backgroundImage = "url('./assets/background-alert-nao-dedo.gif')";
+        return;
+    } else { 
+        customAlert.style.backgroundImage = "url('./assets/background-numero-sorteado.gif')";
+    }
 }
